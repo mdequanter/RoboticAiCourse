c415c0fe50a7ece186cc1735a5409aaa762c78d5
Maarten Dequanter <74420584+mdequanter@users.noreply.github.com>
2022-12-01 11:45:52 +0100

    Add files via upload

---
+++ multiCam.py
@@ -0,0 +1,65 @@
+import cv2
+
+windowName = "StackedCams"
+
+cv2.namedWindow(windowName)
+cam1 = cv2.VideoCapture("rtsp://tapoadmin:ailabo123$@10.3.25.135/stream1")
+cam2 = cv2.VideoCapture("rtsp://tapoadmin:ailabo123$@10.3.25.10/stream1")
+
+
+def stackImages(scale,imgArray):
+    rows = len(imgArray)
+    cols = len(imgArray[0])
+    rowsAvailable = isinstance(imgArray[0], list)
+    width = imgArray[0][0].shape[1]
+    height = imgArray[0][0].shape[0]
+    if rowsAvailable:
+        for x in range ( 0, rows):
+            for y in range(0, cols):
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
+                if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
+        imageBlank = np.zeros((height, width, 3), np.uint8)
+        hor = [imageBlank]*rows
+        hor_con = [imageBlank]*rows
+        for x in range(0, rows):
+            hor[x] = np.hstack(imgArray[x])
+        ver = np.vstack(hor)
+    else:
+        for x in range(0, rows):
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
+            if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
+        hor= np.hstack(imgArray)
+        ver = hor
+    return ver
+
+
+
+def rescale_frame(frame, percent=75):
+    width = int(frame.shape[1] * percent/ 100)
+    height = int(frame.shape[0] * percent/ 100)
+    dim = (width, height)
+    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while True:
+    frame1 = cv2.imread(cam1)
+    frame2 = cv2.imread(cam2)
+    imgStack = stackImages(0.6,([frame1,frame2]))
+    key = cv2.waitKey(1)
+    if key == 27: # exit on ESC
+        break
+
+cam1.release()
+cam2.release()
+cv2.destroyWindow(windowName)
